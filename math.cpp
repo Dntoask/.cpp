ddaa7df41c76363ae4ad327219a17b69f4edf93a
Dntoask <113959153+Dntoask@users.noreply.github.com>
2023-02-11 09:17:54 -0800

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -10,12 +10,14 @@ int main()
     cout << "Input number :" << endl;
     cin >> a;
     
-    cout << "Input number :" << endl;
-    cin >> b;
-    
     cout << "Select operation : - + * RANDOM:" << endl;
     string operation;
     cin >> operation;
+    
+    cout << "Input number :" << endl;
+    cin >> b;
+    
+    
 
     if (operation == "+") {
         cout << "a + b = " << a + b << endl;
@@ -24,9 +26,11 @@ int main()
     }if (operation == "*") {
         cout << "a * b = " << a * b << endl;
     }if (operation == "RANDOM") {
-        cout << "Wait for it! \n " << a * b - b + a << endl;
+        cout << "Wait for it!" << a * b - b + a << endl;
     }if (operation == " / ") {
-        cout << "No such operation" << endl;
+        cout << "This operation is not supported" << endl;
+    }else cout << "operation does not exist.Try again" << endl;
+        return 1;
     }
 return 0;
 }
